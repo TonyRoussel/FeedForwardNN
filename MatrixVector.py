74b102f6855b2816cf74cc2a239947c0bc875f48
shortyp <tonydraf@gmail.com>
2015-08-06 03:31:58 +0200

    add rmul

--- MatrixVector.py
+++ MatrixVector.py
@@ -64,6 +64,9 @@ class Vector(object):
             return Vector(map(operator.mul, self._vector, rhs._vector))
         return Vector([x * rhs for x in self._vector])
 
+    def __rmul__(self, rhs):
+        return Vector([rhs * x for x in self._vector])
+
     def __div__(self, rhs):
         if isinstance(rhs, self.__class__):
             return Vector(map(operator.div, self._vector, rhs._vector))
@@ -203,6 +206,9 @@ class Matrix(object):
             return Matrix(map(lambda l, r: l * r, self._matrix, rhs._matrix))
         return Matrix(map(lambda l: l * rhs, self._matrix))
 
+    def __rmul__(self, rhs):
+        return Matrix(map(lambda l: rhs * l, self._matrix))
+
     def __div__(self, rhs):
         if isinstance(rhs, self.__class__):
             if (self.getSize() != rhs.getSize()):
@@ -268,7 +274,7 @@ if (__name__ == "__main__"):
     print (Matrix.random(3, 4))
     print ("Matrix([Vector([1, 2, 3, 4]), Vector([1, 2, 3, 4]), Vector([1, 2, 3, 4])]) - (Matrix([Vector([1, 2, 3, 4])]))[0] -->")
     print (Matrix([Vector([1, 2, 3, 4]), Vector([1, 2, 3, 4]), Vector([1, 2, 3, 4])]) - (Matrix([Vector([1, 2, 3, 4])]))[0])
-    print ("1 - Matrix.random([Vector([1, 2, 3, 4]))")
+    print ("1 - Matrix([Vector([1, 2, 3, 4]))")
     print (1 - Matrix([Vector([1, 2, 3, 4])]))
     print ("Matrix([Vector([1, 2, 3, 4])) / 2.")
     print (Matrix([Vector([1, 2, 3, 4])]) / 2.)
@@ -282,3 +288,5 @@ if (__name__ == "__main__"):
     print ((Matrix([Vector([1, 2, 3, 4])])).nonlin())
     print ("(Matrix([Vector([1, 2, 3, 4])])).nonlin(True)")
     print ((Matrix([Vector([1, 2, 3, 4])])).nonlin(True))
+    print ("2 * Matrix.random(3, 1) - 1")
+    print (2 * Matrix.random(3, 1) - 1)
