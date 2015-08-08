2b6b64325a7b08bb110b3e3f1cb898ec56ce0a5c
shortyp <tonydraf@gmail.com>
2015-08-09 00:10:59 +0200

    add test nn with bias

--- testNNMatrixVector3LayerAlphaBias.py
+++ testNNMatrixVector3LayerAlphaBias.py
@@ -9,26 +9,28 @@ X = Matrix([Vector([0.,0.,1.]),
            Vector([1.,0.,1.]),
             Vector([1.,1.,1.])])
 
+Xones = Matrix(Matrix.ones(1, X.getRowLen())._matrix + X.transpose()._matrix).transpose()
 y = Matrix([Vector([0.,1.,1.,0.])]).transpose()
 
-# randomly initialize our weights with mean 0
-syn0 = 2 * Matrix.random(3, 4) - 1
-syn1 = 2 * Matrix.random(4, 1) - 1
+# randomly initialize our weights with mean 0 (entry + 1 for bias unit)
+syn0 = 2 * Matrix.random(4, 4) - 1
+syn1 = 2 * Matrix.random(5, 1) - 1
 
 # set the alpha
-alpha = 1
+alpha = 10
 
-for j in xrange(60000):
+for j in xrange(1000):
 
     # Feed forward through layers 0, 1, and 2
-    l0 = X
+    l0 = Xones
     l1 = l0.dotProduct(syn0).nonlin()
+    l1 = Matrix(Matrix.ones(1, l0.getRowLen())._matrix + l1.transpose()._matrix).transpose()
     l2 = l1.dotProduct(syn1).nonlin()
 
     # how much did we miss the target value?
     l2_error = y - l2
 
-    if (j% 10000) == 0:
+    if (j % 100) == 0:
         print "Error:" + str(l2_error.abs().mean())
 
     # in what direction is the target value?
@@ -43,4 +45,10 @@ for j in xrange(60000):
     l1_delta = l1_error * l1.nonlin(True)
 
     syn1 += alpha * (l1.transpose().dotProduct(l2_delta))
+    l1_delta = Matrix(l1_delta.transpose()._matrix[1:]).transpose()
     syn0 += alpha * (l0.transpose().dotProduct(l1_delta))
+
+print "output after training"
+l1 = (Xones.dotProduct(syn0)).nonlin()
+l1 = Matrix(Matrix.ones(1, l1.getRowLen())._matrix + l1.transpose()._matrix).transpose()
+print l1.dotProduct(syn1).nonlin()
