d774fc57d1b2316f4e4157bb85d5c59051cfe409
Tony R <tonydraf@gmail.com>
2015-08-09 15:33:58 +0200

    Backprop training in progress

--- FeedForwardNeuralNetwork.py
+++ FeedForwardNeuralNetwork.py
@@ -49,6 +49,37 @@ class FeedForwardNN(object):
             self._layer_output.append(layer_input.nonlin())
         return self._layer_output[-1]
 
+    def _measure_deltas(self, y):
+        """ Given the last outputs, calculate for each layer weights the distance to target """
+
+        deltas = []
+        for idx, output in enumerate(reversed(self._layer_output)):
+            if idx == 0:
+                l_error = y - output
+            else:
+                l_error = deltas[-idx].dotProduct(self._weights[-idx].transpose())
+            # print "len(self._layer_output)"
+            # print len(self._layer_output)
+            # print "l_error.getSize()"
+            # print l_error.getSize()
+            # print "output.nonlin(True).getSize()"
+            # print output.nonlin(True).getSize()
+            deltas = [l_error * output.nonlin(True)] + deltas
+        return deltas
+
+    def backpropagation_training(self, X, y, alpha=0.1, epoch=100):
+        """ Train the neural net with the backpropagation algorithm
+        return final error """
+
+        for epk in xrange(0, epoch):
+            self.run(X)
+            # for each layer output calculate distance to target
+            deltas = self._measure_deltas(y)
+            # move the weights toward target with alpha step
+            # self._proceed_weights_step(alpha, deltas) #############
+        # return last error
+        return ((y - self._layer_output[-1]).abs().mean())
+
 
 
 if __name__ == "__main__":
@@ -56,6 +87,7 @@ if __name__ == "__main__":
                 Vector([0.,1.,1.]),
                 Vector([1.,0.,1.]),
                 Vector([1.,1.,1.])])
+    y = Matrix([Vector([0.,1.,1.,0.])]).transpose()
     ffnn = FeedForwardNN([3, 4, 1])
     print "ffnn._layers_shape"
     print ffnn._layers_shape
@@ -66,3 +98,5 @@ if __name__ == "__main__":
         print layer
     print "ffnn.run(X)"
     print ffnn.run(X)
+    print "ffnn.backpropagation_training(X, y)"
+    print ffnn.backpropagation_training(X, y)
