38ed8ef978e6b1325049b68e1c93ef3813a8f3aa
Tony ROUSSEL <troussel@e3r9p5.42.fr>
2015-08-13 16:40:55 +0200

    Dota2: add function to extract training data binary form

--- Dota2_nn/main.py
+++ Dota2_nn/main.py
@@ -57,6 +57,37 @@ def read_training_data_one_of_n():
                 y.append([0, 1])
     return X, y, summon_names
 
+def read_training_data_binary():
+    with open("trainingdata.txt", 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        summon_names = dict()
+        for i, row in enumerate(reader):
+            if i == 0:
+                ncols = len(row)
+            for value in row[0:ncols - 1]:
+                if value not in summon_names:
+                    if len(summon_names) == 0:
+                        summon_names[value] = 0
+                    else:
+                        summon_names[value] = len(summon_names)
+    binary_base = len(bin(summon_names[max(summon_names, key=summon_names.get)])) - 2
+    format_guide = '{0:0' + str(binary_base) + 'b}'
+    with open("trainingdata.txt", 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        X = list()
+        y = list()
+        for i, row in enumerate(reader):
+            line = list()
+            for value in row[0:ncols - 1]:
+                binary_representation = [int(x) for x in format_guide.format(summon_names[value])]
+                line = line + binary_representation
+            X.append(line)
+            if int(row[ncols - 1]) == 1:
+                y.append([1, 0])
+            else:
+                y.append([0, 1])
+    return X, y, summon_names
+
 
 def normalizer(X):
     return X.sum(axis=1)
@@ -66,7 +97,8 @@ def normalize(mtx, norm):
 
 
 # X, y, d = read_training_data()
-Xl, yl, d = read_training_data_one_of_n()
+# Xl, yl, d = read_training_data_one_of_n()
+Xl, yl, d = read_training_data_binary()
 
 # # crossvalidation data construction RANDOM PICK
 Xt = Xl
@@ -85,14 +117,13 @@ X = np.array(Xt)
 y = np.array(yt)
 Xp = np.array(Xp)
 yp = np.array(yp)
-
 # norm = normalizer(X)
 ffnn = FeedForwardNN([len(X[0]), (len(X[0]) + len(y[0])) / 2, len(y[0])], hidden_layer="tanh", output_layer="tanh", input_layer="tanh")
 
 # ffnn.backpropagation_training(normalize(X, norm), y, alpha=0.00001, epoch=100, momentum=0.99, plot_error=True)
-# ffnn.backpropagation_training(X, y, alpha=1e-7, epoch=4000, momentum=0.99, plot_error=True)
+# ffnn.backpropagation_training(X, y, alpha=1e-7, epoch=8000, momentum=0.99, plot_error=True)
 # ffnn.adadelta_training(normalize(X, norm), y, epoch=100, plot_error=True)
-ffnn.adadelta_training(X, y, epoch=4000, plot_error=True)
+ffnn.adadelta_training(X, y, epoch=8000, plot_error=True)
 
 # prediction = ffnn.run(normalize(X, norm))
 prediction = ffnn.run(Xp)
