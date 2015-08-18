59b801725f25554a7dc7a6deab9d7e4f58fb3801
Tony ROUSSEL <troussel@e2r8p16.42.fr>
2015-08-18 15:07:32 +0200

    add lambda regularization parameter in error plot name

--- FeedForwardNeuralNetworkNumpy.py
+++ FeedForwardNeuralNetworkNumpy.py
@@ -135,7 +135,7 @@ class FeedForwardNN(object):
         verbose_cycle = 0.01 * epoch
 
         if plot_error:
-            plot_name = "BPT_shape" + str(self._layers_shape) + "_types|" + self._input_layer_type + "|" + self._hidden_layer_type + "|" + self._output_layer_type + "_epoch" + str(epoch) + "_alpha" + str(alpha) + "_mom" + str(momentum)
+            plot_name = "BPT_shape" + str(self._layers_shape) + "_types|" + self._input_layer_type + "|" + self._hidden_layer_type + "|" + self._output_layer_type + "_epoch" + str(epoch) + "_alpha" + str(alpha) + "_mom" + str(momentum) + "_lregu" + str(l_regularization)
             error_history = []
             epoch_history = []
             fig = plt.figure()
@@ -172,7 +172,7 @@ class FeedForwardNN(object):
         verbose_cycle = 0.01 * epoch
 
         if plot_error:
-            plot_name = "SGD_shape" + str(self._layers_shape) + "_types|" + self._input_layer_type + "|" + self._hidden_layer_type + "|" + self._output_layer_type + "_epoch" + str(epoch) + "_alpha" + str(alpha) + "_mom" + str(momentum) + "_batchsize" + str(mini_batch_size)
+            plot_name = "SGD_shape" + str(self._layers_shape) + "_types|" + self._input_layer_type + "|" + self._hidden_layer_type + "|" + self._output_layer_type + "_epoch" + str(epoch) + "_alpha" + str(alpha) + "_mom" + str(momentum) + "_lregu" + str(l_regularization) + "_batchsize" + str(mini_batch_size)
             error_history = []
             epoch_history = []
             fig = plt.figure()
@@ -230,7 +230,7 @@ class FeedForwardNN(object):
         verbose_cycle = 0.01 * epoch
 
         if plot_error:
-            plot_name = "ADAD_shape" + str(self._layers_shape) + "_types|" + self._input_layer_type + "|" + self._hidden_layer_type + "|" + self._output_layer_type + "_epoch" + str(epoch)
+            plot_name = "ADAD_shape" + str(self._layers_shape) + "_types|" + self._input_layer_type + "|" + self._hidden_layer_type + "|" + self._output_layer_type + "_epoch" + str(epoch) + "_lregu" + str(l_regularization)
             error_history = []
             epoch_history = []
             fig = plt.figure()
