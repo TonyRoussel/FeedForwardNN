0d7db1c424c5366d466428c45b900bec966d6147
Tony R <tonydraf@gmail.com>
2015-08-29 13:27:03 +0200

    add mnist_bench v1

--- mnist_bench.py
+++ mnist_bench.py
@@ -1,8 +1,83 @@
+import operator
+import numpy as np
 from MNIST_db import mnist_numpy
+from FeedForwardNeuralNetworkNumpy import FeedForwardNN
 
 from pylab import *
-from numpy import *
 
-images, labels = mnist_numpy.load_mnist('training', digits=[2])
-imshow(images.mean(axis=0), cmap=cm.gray)
-show()
+def convert_mnist_2d_1ToNOutput(imgs, labels):
+    output_map =\
+    [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+    X = list()
+    y = list()
+    for idx, img in enumerate(imgs):
+        X.append(list(img.flatten()))
+        y.append(output_map[labels[idx][0] - 1])
+    return X, y
+
+images_training, labels_training = mnist_numpy.load_mnist('training')
+images_test, labels_test = mnist_numpy.load_mnist('testing')
+
+images_training /= 255.
+images_test /= 255.
+
+# print "images.shapes"
+# print images.shape
+# print "labels.shapes"
+# print labels.shape
+# print "labels[0]"
+# print labels[0]
+# print "images[0].shape"
+# print images[0].shape
+# print "images[0].flatten().shape"
+# print images[0].flatten().shape
+# imshow(images.mean(axis=0), cmap=cm.gray)
+# show()
+
+Xt, yt = convert_mnist_2d_1ToNOutput(images_training, labels_training)
+Xp, yp = convert_mnist_2d_1ToNOutput(images_test, labels_test)
+
+X = np.array(Xt)
+y = np.array(yt)
+Xp = np.array(Xp)
+yp = np.array(yp)
+
+ffnn = FeedForwardNN([len(X[0]), len(X[0]), len(y[0])], hidden_layer="sigmoid", output_layer="softmax", input_layer="sigmoid")
+
+ffnn.backpropagation_training(X, y, alpha=1e-7, epoch=100, momentum=0.99, plot_error=True)
+# ffnn.SGD_training(X, y, alpha=1e-7, epoch=5000, momentum=0.99, mini_batch_size=10, plot_error=True)
+# ffnn.adadelta_training(X, y, epoch=100, l_regularization=0.1, plot_error=True)
+
+
+prediction = ffnn.run(X)
+total = len(y)
+count = 0
+for i in xrange(total):
+    indexp = max(enumerate(prediction[i]), key=operator.itemgetter(1))[0]
+    indexe = max(enumerate(y[i]), key=operator.itemgetter(1))[0]
+    if indexp == indexe:
+        count = count + 1
+print "Final rate on training set:", count / float(total)
+
+
+prediction = ffnn.run(Xp)
+total = len(yp)
+count = 0
+for i in xrange(total):
+    indexp = max(enumerate(prediction[i]), key=operator.itemgetter(1))[0]
+    indexe = max(enumerate(yp[i]), key=operator.itemgetter(1))[0]
+    if indexp == indexe:
+        count = count + 1
+print "Final rate on validation set:", count / float(total)
+
+raw_input('waiting to close graph')
+
