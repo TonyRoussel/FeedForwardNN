c7a9d9dcf5828c3aeef76373fd0401edf972f149
shortyp <tonydraf@gmail.com>
2015-08-30 03:34:44 +0200

    mnist_bench: finish moving test data loading after training phase

--- mnist_bench.py
+++ mnist_bench.py
@@ -25,10 +25,6 @@ def convert_mnist_2d_1ToNOutput(imgs, labels):
     return X, y
 
 images_training, labels_training = mnist_numpy.load_mnist('training')
-images_test, labels_test = mnist_numpy.load_mnist('testing')
-
-images_training /= 255.
-images_test /= 255.
 
 # print "images.shapes"
 # print images.shape
@@ -43,6 +39,8 @@ images_test /= 255.
 # imshow(images.mean(axis=0), cmap=cm.gray)
 # show()
 
+images_training /= 255.
+
 Xt, yt = convert_mnist_2d_1ToNOutput(images_training, labels_training)
 
 X = np.array(Xt)
@@ -54,6 +52,8 @@ ffnn.backpropagation_training(X, y, alpha=3, epoch=100, momentum=0., l_regulariz
 # ffnn.SGD_training(X, y, alpha=1e-7, epoch=5000, momentum=0.99, mini_batch_size=10, plot_error=True)
 # ffnn.adadelta_training(X, y, epoch=100, l_regularization=0, plot_error=True)
 
+images_test, labels_test = mnist_numpy.load_mnist('testing')
+images_test /= 255.
 Xp, yp = convert_mnist_2d_1ToNOutput(images_test, labels_test)
 Xp = np.array(Xp)
 yp = np.array(yp)
